74d82e80f7fae0582eddb5a14747a7d510e89c63
KDG <81103357+Kim-DG@users.noreply.github.com>
2024-11-24 16:56:44 +0900

    rebase 수정

--- presentation/src/main/java/com/keunsori/presentation/MainActivity.kt
+++ presentation/src/main/java/com/keunsori/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package com.keunsori.presentation
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,28 +15,22 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.credentials.CredentialManager
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavOptions
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.keunsori.presentation.intent.MainEffect
 import com.keunsori.presentation.intent.LoginEffect
-import com.keunsori.presentation.intent.LoginEvent
-import com.keunsori.presentation.viewmodel.LoginViewModel
-import com.keunsori.presentation.ui.LoginScreen
-import com.keunsori.presentation.viewmodel.MainViewModel
+import com.keunsori.presentation.intent.MainEffect
 import com.keunsori.presentation.ui.InGameScreen
-import com.keunsori.presentation.ui.SettingScreen
+import com.keunsori.presentation.ui.LoginScreen
 import com.keunsori.presentation.ui.MainScreen
-import com.keunsori.presentation.ui.main.ChooseLevelScreen
+import com.keunsori.presentation.ui.SettingScreen
 import com.keunsori.presentation.ui.theme.TowerdleTheme
 import com.keunsori.presentation.utils.LocalCredentialManagerController
 import com.keunsori.presentation.utils.MyCredentialManagerController
 import com.keunsori.presentation.utils.Navigation
 import com.keunsori.presentation.viewmodel.InGameViewModel
+import com.keunsori.presentation.viewmodel.LoginViewModel
+import com.keunsori.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
