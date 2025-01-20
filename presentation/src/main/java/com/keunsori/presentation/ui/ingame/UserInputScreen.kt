79cf5f076b94bf9f1ef18e29ed18b5fb531b4fcd
SeungYeon Kim <seungyeon.kim@testbank.ai>
2025-01-20 14:06:14 +0900

    Update UserInputScreen.kt

--- presentation/src/main/java/com/keunsori/presentation/ui/ingame/UserInputScreen.kt
+++ presentation/src/main/java/com/keunsori/presentation/ui/ingame/UserInputScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +19,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
 import com.keunsori.presentation.R
 import com.keunsori.presentation.model.UserInput
 
@@ -61,8 +62,9 @@ fun UserInputScreen(
 @Composable
 private fun UserTrialRow(input: UserInput?, trialOnGoing: Boolean, quizSize: Int) {
     Row(
-        modifier = Modifier.height(45.dp).wrapContentWidth(),
-        verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier.height(45.dp).width(quizSize * 45.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(0.dp)
     ) {
         for (i in 0..<quizSize) {
             val element = try {
