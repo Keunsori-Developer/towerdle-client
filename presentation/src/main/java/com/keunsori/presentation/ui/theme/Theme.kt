a286627809580a9054e4405234b514725d962784
SeungYeon Kim <k_sy_3@naver.com>
2025-04-27 14:12:36 +0900

    dynamic color scheme 제거

--- presentation/src/main/java/com/keunsori/presentation/ui/theme/Theme.kt
+++ presentation/src/main/java/com/keunsori/presentation/ui/theme/Theme.kt
@@ -94,16 +94,9 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun TowerdleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicLightColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> LightColorScheme
         else -> LightColorScheme
     }
